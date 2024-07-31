a1468195833d9a88ebd3c617306cc03d7da40c72
Shailendra S. Maurya <162585603+shailendrabt@users.noreply.github.com>
2024-07-31 13:59:55 +0530

    Update plot_graph.R

--- plot_graph.R
+++ plot_graph.R
@@ -4,3 +4,16 @@ library(ggplot2)
 x <- seq(-10,10,0.1)
 y<-x^2
 qplot(x,y, geom = "line")
+
+###exercise 2
+##plot a graph with sine x on the y-axis and x on the x-axis
+library(ggplot2)
+x <- seq(0,8*pi,0.1)
+y<-sin(x)
+qplot(x,y, geom = "line")
+
+###exercise 3
+### plot a histogram of 1000 random normal deviates
+library(ggplot2)
+x <- rnorm(1000)
+qplot(x)
