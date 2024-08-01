a1bb5dde16f2e0374be28ebb487e2fca51498da7
Shailendra S. Maurya <162585603+shailendrabt@users.noreply.github.com>
2024-08-01 14:18:16 +0530

    Update ggplot.R

--- ggplot.R
+++ ggplot.R
@@ -5,6 +5,11 @@ rm(list = ls())
 getwd()
 compensation <- read.csv("compensation.csv")
 ggplot(compensation, aes(x= Root, y = Fruit)) + 
-  geom_point()
+  geom_point() +
+  theme_bw()
 
-theme_bw()
+ggplot(compensation, aes(x = Root, y = Fruit, colour = Grazing)) +
+  geom_point(size = 5) +
+  xlab("Root Biomass") +
+  ylab("Fruit Production") +
+  theme_bw()
