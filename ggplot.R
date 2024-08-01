967b6c86c1a843c1a1aedbedee4a1848c1e21a81
Shailendra S. Maurya <162585603+shailendrabt@users.noreply.github.com>
2024-08-01 15:51:20 +0530

    Update ggplot.R

--- ggplot.R
+++ ggplot.R
@@ -13,3 +13,38 @@ ggplot(compensation, aes(x = Root, y = Fruit, colour = Grazing)) +
   xlab("Root Biomass") +
   ylab("Fruit Production") +
   theme_bw()
+
+library(dplyr)
+library(ggplot2)
+rm(list = ls())
+getwd()
+setwd("datasets-master/")
+compensation <- read.csv("compensation.csv")
+glimpse(compensation)
+ggplot(compensation, aes(x= Root, y = Fruit)) + 
+  geom_point()
+library(ggplot2)
+theme_bw()
+getwd()
+ggplot(compensation, aes(x= Root, y = Fruit)) + 
+  geom_point(size = 5) +
+  theme_bw()
+ggplot(compensation, aes(x = Root, y = Fruit, colour = Grazing)) +
+  geom_point(size = 5) +
+  xlab("Root Biomass") +
+  ylab("Fruit Production") +
+  theme_bw()
+
+compensation <- read.csv("compensation.csv")
+glimpse(compensation)
+ggplot(compensation, aes(x = Root, y = Fruit, colour = Grazing)) +
+  geom_point(size = 5) +
+  xlab("Root Biomass") +
+  ylab("Fruit Production") +
+  theme_bw()
+library(ggplot2)
+
+ggplot(compensation, aes(x = Fruit))+
+  geom_histogram(bins = 30)
+ggplot(compensation, aes(x = Fruit))+
+  geom_histogram(binwidth = 15)
