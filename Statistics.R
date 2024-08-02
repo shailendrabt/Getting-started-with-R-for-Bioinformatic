f630198296b9423b64b961b3472e19860d4afd05
Shailendra S. Maurya <162585603+shailendrabt@users.noreply.github.com>
2024-08-02 12:31:10 +0530

    Update Statistics.R

--- Statistics.R
+++ Statistics.R
@@ -1,4 +1,5 @@
-####Chi square contigency analysis
+                                          ####Chi square contigency analysis
+
 library(dplyr)
 library(ggplot2)
 ###import the data
@@ -27,3 +28,40 @@ lady.mat
 chisq.test(lady.mat)
 lady.chi <- chisq.test(lady.mat)
 names(lady.chi)
+
+                                         #########two sample t-test 
+###load data
+getwd()
+ozone <- read.csv("ozone.csv")
+glimpse(ozone)
+###two sample test in one effort
+ggplot(ozone, aes(x = Ozone)) +
+  geom_histogram(binwidth = 10) +
+  facet_wrap(~ Garden.location, ncol = 1) +
+  theme_bw()
+> t.test(Ozone ~ Garden.location, data = ozone)
+output #[] Welch Two Sample t-test
+
+data:  Ozone by Garden.location
+t = 4.2363, df = 17.656, p-value = 0.0005159
+alternative hypothesis: true difference in means between group East and group West is not equal to 0
+95 percent confidence interval:
+  8.094171 24.065829
+sample estimates:
+mean in group East mean in group West 
+             77.34              61.26 
+
+
+> var.test(Ozone ~ Garden.location, data = ozone)
+
+output (##	F test to compare two variances
+
+data:  Ozone by Garden.location
+F = 0.75503, num df = 9, denom df = 9, p-value = 0.6823
+alternative hypothesis: true ratio of variances is not equal to 1
+95 percent confidence interval:
+ 0.1875386 3.0397437
+sample estimates:
+ratio of variances 
+         0.7550293 )
+
