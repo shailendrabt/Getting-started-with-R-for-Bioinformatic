72274d38b5e8d67c033e474f65a2de3a8a8efe86
Shailendra S. Maurya <162585603+shailendrabt@users.noreply.github.com>
2024-08-05 12:08:02 +0530

    Update Adavace_statistics.R

--- Adavace_statistics.R
+++ Adavace_statistics.R
@@ -23,3 +23,72 @@ ggplot(sumMoo, aes(x= supplement, y = meanGrow,
 ###specifying this trick formula and the data frame
 model_cow <- lm(gain ~diet * supplement, data = growth.moo)
 ###so lets build those four plots
+autoplot(model_cow, smooth.colour = NA)
+
+
+library(ggfortify)
+####model output and more biology
+anova(model_cow)
+####for summary table
+summary(model_cow)
+
+
+####STATS BACK TO GRAPHICS
+###calculate mean and sd of gain for all 12 combinations
+sumMoo <- growth.moo  %>%
+  group_by(diet, supplement) %>%
+  summarise(
+    meanGrow = mean(gain),
+    seGrow = sd(gain) / sqrt(n())
+  )
+ggplot(sumMoo, aes(x = supplement, y = meanGrow,
+                   color = diet, group = diet)) +
+  geom_point() +
+  geom_line() +
+  geom_errorbar(aes(ymin = meanGrow - seGrow,
+                    ymax=meanGrow + seGrow),width = 0.1) +
+  theme_bw()
+###Analysis of covariance(ANCOVA)
+limp <- read.csv("limpet.csv")
+glimpse(limp)
+###plot window
+ggplot(limp, aes(x = DENSITY, y= EGGS, colour = SEASON)) +
+  geom_point() +
+  scale_color_manual(values = c(spring = "green", summer = "red"))
+####construct the ancova
+limp.md <- lm(EGGS ~ DENSITY * SEASON, data = limp)
+names(limp.md)
+autoplot(limp.md, smooth.colour = NA)
+anova(limp.md)
+summary(limp.md)
+
+predict(limp.md)
+####make some new density values at which we request prediction
+new.x <- expand.grid(DENSITY =
+                       seq(from = 8, to = 45, length.out = 10))
+###check it
+head(new.x)
+####make some new density values at which we request predictions
+new.x <- expand.grid( 
+  DENSITY = seq(from = 8, to = 45, length.out = 10),
+  SEASON = levels(limp$SEASON)) 
+###check it
+head(new.x)
+
+####generate fits and confidence interval at new.x values.
+new.y <- predict(limp.md, newdata=new.x, 
+                 interval = "confidence")
+
+###the final picture using ggplot2
+
+###raw data plot(you dont need to write this again)
+ggplot(limp, aes(x = DENSITY, y = EGGS, colour = SEASON))+
+###first add the point
+  geom_point(size = 5) +
+  ###now add the fits and CIs 
+  ###note we dont need to specify DENSITY AND EGGS 
+  ##the are inherited from above
+  addThese <- read.csv("addThese")
+  geom_smooth(data = addThese, 
+              aes(ymin = lwr, ymax = upr,
+                  fill = SEASON), stat = "identity")
