263853d974be6ff9063680f29c7f478bf4b4162f
Shailendra S. Maurya <162585603+shailendrabt@users.noreply.github.com>
2024-08-06 12:39:31 +0530

    Update ggplot2.R

--- ggplot2.R
+++ ggplot2.R
@@ -22,3 +22,32 @@ eg_scatter +
            label = c("Text up here...",  "and  text down here")
 eg_scatter + scale_alpha_continuous(limits = c(4, 11), breaks = 4:11)
 ggplot(data = compensation, aes(x = Root, y = Fruit, colour = Grazing))          
+ geom_point() +
+  scale_color_manual(values = c(Grazed = "brown", Ungrazed = 
+                                  "green"))
+eg_box + 
+  scale_y_continuous(breaks = seq(from = 10, to = 150, by = 20),
+                     trans = "log10")
+####the theme
+eg_scatter +
+  theme(
+    panel.background = element_rect(fill = NA, colour = "red"),
+    panel.grid.minor = element_blank(),
+    panel.grid.major = element_line(colour = "black")
+  )
+####some theme() syntax about axis title and tick marks
+eg_box +
+  theme(
+    axis.title.x = element_text(colour = "cornflowerblue",
+    size = rel(2)),
+    axis.text.x = element_text(angle = 45, size = 13, vjust = 0.5)
+  )
+eg_box + scale_x_discrete(limits = c("Ungrazed", "Grazed"),
+                          labels(c("Control", "Grazed"))
+ggplot(compensation, aes(x = Root, y = Fruit, colours(Grazing)) +
+         geom_point() +
+         theme(legand.key = element_rect(fill = NA))
+       
+ggplot(compensation, aes(x = Root, y = Fruit, colour = Grazing)) +
+  geom_point() +
+  theme(legand.position = "none")
